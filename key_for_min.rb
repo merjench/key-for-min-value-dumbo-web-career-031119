c7ac94af64d9b407e502798dacc4e86e13ccb1e1
Merjen Charyyeva <merjen.chary@gmail.com>
2019-02-28 00:39:28 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,13 @@
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
  
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value| 
-    if min_value == nil 
-      min_value = key 
-    elsif value.to_i < name_hash[min_value].to_i
-      min_value = key 
-    end 
+  min_key = nil
+  min_value = nil
+  name_hash.each do |k, v|
+    if min_value == nil || v < min_value
+      min_value = v
+      min_key = k
+    end
   end
-  min_value
-  end 	
-end
+  min_key
+end	
